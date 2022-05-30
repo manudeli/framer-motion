b72b19d2624fbdd8adc2fd220fb7e6e6f3ad9150
마누 <manu.k@kakaocorp.com>
2022-05-30 22:43:08 +0900

    feat(framer-motion): when: 'beforeChildren' 추가 애니메이션 시간차 보여주기

--- src/pages/Order/index.tsx
+++ src/pages/Order/index.tsx
@@ -1,3 +1,41 @@
+import { motion, Variants } from "framer-motion";
+
+const containerVariants: Variants = {
+  hidden: {
+    rotateY: 300,
+    opacity: 0,
+    x: "50vw",
+  },
+  visible: {
+    rotateY: 0,
+    opacity: 1,
+    x: 0,
+    transition: {
+      type: "spring",
+      when: "beforeChildren",
+    },
+  },
+  boom: {
+    rotateY: 0,
+    scale: 0.9,
+    transition: {
+      type: "spring",
+    },
+  },
+};
+
+const childVariants: Variants = {
+  hidden: {
+    opacity: 0,
+    y: 20,
+  },
+  visible: {
+    opacity: 1,
+    textShadow: "0 0 8px #f8e112",
+    scale: 1.2,
+    y: 0,
+  },
+};
 interface Props {
   pizza: {
     base: string;
@@ -9,13 +47,29 @@ const Order = ({ pizza }: Props) => {
   const { base, toppings } = pizza;
 
   return (
-    <div className="container order">
+    <motion.div
+      className="container order"
+      variants={containerVariants}
+      initial="hidden"
+      animate="visible"
+    >
       <h2>Thank you for your order </h2>
-      <p>You ordered a {base} pizza with:</p>
-      {toppings.map((topping) => (
-        <div key={topping}>{topping}</div>
-      ))}
-    </div>
+      <motion.p initial="hidden" animate="visible">
+        You ordered a <motion.span variants={childVariants}>{base}</motion.span>{" "}
+        pizza with:
+      </motion.p>
+      <motion.div>
+        {toppings.map((topping, index) => (
+          <motion.div
+            key={topping}
+            variants={childVariants}
+            transition={{ delay: index * 0.1 }}
+          >
+            {topping}
+          </motion.div>
+        ))}
+      </motion.div>
+    </motion.div>
   );
 };
 
