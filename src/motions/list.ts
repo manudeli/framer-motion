3dc011c246406a872c9e2a3cd651ffd58253941b
마누 <manu.k@kakaocorp.com>
2022-05-30 22:18:26 +0900

    fix(list): list 스타일 default / selected / hover 모션 추가

--- src/motions/list.ts
+++ src/motions/list.ts
@@ -1,1 +1,9 @@
-export const selectedListStyle = { scale: 1.3, originX: 0, color: "#f8e112" };
+import { TargetAndTransition } from "framer-motion";
+
+export const selectedListStyle: TargetAndTransition = {
+  scale: 1.2,
+  originX: 0,
+  color: "#ffffff",
+  textShadow: "0 0 8px #f8e112",
+  fontWeight: "bold",
+} as const;
--- src/pages/Base/index.tsx
+++ src/pages/Base/index.tsx
@@ -30,7 +30,7 @@ const Base = ({ addBase, pizza }: Props) => {
               key={base}
               onClick={() => addBase(base)}
               animate={pizza.base === base ? selectedListStyle : {}}
-              whileHover={selectedListStyle}
+              whileHover={{ scale: 1.3, originX: 0, color: "#f8e112" }}
               transition={{ type: "spring", stiffness: 600 }}
             >
               <span className={spanClass}>{base}</span>
