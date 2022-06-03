8c767643da480cbc29807e38bf4e7e214136f49c
마누 <manu.k@kakaocorp.com>
2022-06-04 01:40:53 +0900

    feat(framer-motion): keyframe 추가

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,18 @@
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
 
+const buttonVariants = {
+  visible: {
+    x: [0, 20, -20, 20, -20, 20, 0],
+    transition: { delay: 2 },
+  },
+  hover: {
+    scale: [1, 1.1, 1, 1.1, 1, 1.1, 1],
+    textShadow: "0px 0px 8px rgb(255,255,255)",
+    boxShadow: "0px 0px 8px rgb(255,255,255)",
+  },
+};
+
 const Home = () => {
   return (
     <motion.div
@@ -12,12 +24,9 @@ const Home = () => {
       <motion.h2>Welcome to Pizza Joint</motion.h2>
       <Link to="/base">
         <motion.button
-          animate={{ scale: 1 }}
-          whileHover={{
-            scale: 1.1,
-            textShadow: "0px 0px 8px rgb(255,255,255)",
-            boxShadow: "0px 0px 8px rgb(255,255,255)",
-          }}
+          variants={buttonVariants}
+          animate="visible"
+          whileHover="hover"
         >
           Create Your Pizza
         </motion.button>
